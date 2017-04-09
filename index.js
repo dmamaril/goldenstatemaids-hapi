52cf0a3a19297c5b59cb5933bd1afd5488ff53e1
Don Mamaril <mamarildon@gmail.com>
2017-04-09 01:22:08 -0700

    [update] schemas: customer schema change book_history from array to obj to avoid anti pattern

--- index.js
+++ index.js
@@ -7,15 +7,13 @@ import customers from './lib/customers';
 require('dotenv').config();
 
 // INITIALIZE FIREBASE;
-const cfg = {
+firebase.initializeApp({
     apiKey             : process.env.FIREBASE_API_KEY,
     authDomain         : process.env.FIREBASE_AUTH_DOMAIN,
     databaseURL        : process.env.FIREBASE_DATABASE_URL,
     storageBucket      : process.env.FIREBASE_STORAGE_BUCKET,
     messagingSenderId  : process.env.FIREBASE_MESSAGING_SENDER_ID
-};
-
-firebase.initializeApp(cfg);
+});
 
 // INITIALIZE STRIPE
 const stripe = stripePkg(process.env.STRIPE_SECRET);
